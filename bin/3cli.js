f5525e97c49a1e51f0476c0f570e5f14477ab173
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-06-26 15:31:34 -0600

    update to 0.0.27

--- bin/3cli.js
+++ bin/3cli.js
@@ -12,7 +12,6 @@ var Profile = require("./profile")
 var App = require("./app")
 var Server = require("../app/actions/server")
 var Log = require("3vot-cloud/utils/log")
-var Stat = require("3vot-cloud/utils/stats")
 
 Log.setLevel("INFO")
 
